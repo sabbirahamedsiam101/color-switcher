d230c3c1f3b397a36068f3231c31cb43039214fe
sabbirahamedsiam101 <sabbirahamedsiam101@gmail.com>
2023-10-31 20:21:55 +0600

    change

---
+++ index.js
@@ -0,0 +1,27 @@
+let button = document.querySelectorAll(".button");
+// console.log(button)
+let body= document.querySelector("body");
+
+button.forEach((button)=>{
+    // console.log(button)
+    button.addEventListener("click", (e)=>{
+    console.log(e);
+    console.log(e.target);
+    if (e.target.id === "grey") {
+        body.style.backgroundColor = e.target.id;
+    }
+    if (e.target.id === "red") {
+        body.style.backgroundColor = e.target.id;
+    }
+    if (e.target.id === "blue") {
+        body.style.backgroundColor = e.target.id;
+    }
+    if (e.target.id === "yellow") {
+        body.style.backgroundColor = e.target.id;
+    }
+    if (e.target.id === "grey") {
+        body.style.backgroundColor = e.target.id;
+    }
+}
+    )
+})
